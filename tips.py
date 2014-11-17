4aa45eb13230c9ebbbfd6d008eb4fe55f1dca2c3
felgari <felgari@gmail.com>
2014-11-17 20:28:54 +0100

    New python tips.

--- tips.py
+++ tips.py
@@ -1,3 +1,7 @@
+##### Lists.
+# Insert at the beginning of a list.
+my_list(o, item)
+
 # Flatten a list.
 [item for sublist in here_my_list for item in sublist]
 
@@ -12,3 +16,9 @@ here_my_numpy_array[here_my_numpy_array[:,here_my_column_number].argsort()]
 
 # Extract a column from a numpy array (column is zero based).
 here_my_numpy_array[:,column_number]
+
+# Getting a string of file names from a list containing the file names.
+list_of_files = str(files).translate(None, "[]\'")
+
+# Getting indexes to iterate over a list in reverse order.
+range(len(my_list)-1,-1,-1)
